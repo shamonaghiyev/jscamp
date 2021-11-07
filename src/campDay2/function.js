da3f1d239a0078fe9c8f96d551b8165ba0bf2e70
Shamo <nagyev2@gmail.com>
2021-11-08 00:17:29 +0400

    create sayyHello Function

--- src/campDay2/function.js
+++ src/campDay2/function.js
@@ -1,5 +1,21 @@
-function addToCart(productName) {
-    console.log("Səbətə Əlavə edildi! :" + productName);
+function addToCart(productName = "Apple Watch", quantity) {
+    console.log("Səbətə Əlavə edildi! - məhsul : " + productName + ", ədəd : " + quantity);
 }
 
-addToCart();
+addToCart("",6);
+addToCart("iPhone", 9);
+addToCart("iPad", 20);
+addToCart("MacBook", 2);
+
+
+let sayHello = ()=> {
+    console.log("Hello World!");
+}
+
+sayHello();
+
+let sayHello2 = function () {
+    console.log("Hello World 2!");
+}
+
+sayHello2();
