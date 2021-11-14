50afab88f3e315b2508d03a93430ba512757008b
Shamo Naghiyev <nagyev2@gmail.com>
2021-11-14 20:35:43 +0400

    start campDay3

---
+++ src/campDay3/mapFilterReduce.js
@@ -0,0 +1,8 @@
+let cart = [
+    {id:1, productName:"iPhone", quantity:2, unitPrice:2000},
+    {id:2, productName:"MacBook", quantity:1, unitPrice:4000},
+    {id:3, productName:"Airpods", quantity:9, unitPrice:400},
+    {id:4, productName:"iPad", quantity:4, unitPrice:1400},
+    {id:5, productName:"Apple Watch", quantity:3, unitPrice:1100},
+]
+// SPA Single Page Application
