d0458f034b57a892ecb7467484f5079ca52a5d9b
Shamo Naghiyev <nagyev2@gmail.com>
2021-11-15 22:25:04 +0400

    end Camp 3 Days

---
+++ src/campDay3/components/userComponent.js
@@ -0,0 +1,21 @@
+import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logger.js";
+import User from "../models/user.js";
+import UserService from "../services/userService.js"
+
+
+let logger1 = new ElasticLogger();
+let userService = new UserService(logger1);
+
+let user1 = new User(1,"Lenovo","Legion","Tovuz");
+let user2 = new User(2,"Apple","MacBook","Baku");
+
+userService.add(user1);
+userService.add(user2);
+console.log(userService.list());
+console.log(userService.getById(2));
+
+let customer = { id: 1, firstName: "Shamo" };
+//Prototyping
+customer.lastName = "Naghiyev";
+
+console.log(customer.lastName);
